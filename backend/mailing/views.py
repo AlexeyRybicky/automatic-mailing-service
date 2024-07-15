9123028499f56716faf81f73c319e60f4d90eb94
Aleksey <ranangariel@gmail.com>
2024-07-15 10:52:27 +0300

    Добавил документацию к viewsets

--- backend/mailing/views.py
+++ backend/mailing/views.py
@@ -1,27 +1,46 @@
 """В модуле содержаться классы представлений для приложения `mailing`"""
 
-
 from rest_framework import viewsets
 
 from mailing.models import Client, Mailing, Message, Tag
 from mailing.serializers import ClientSerializer, MailingSerializer, MessageSerializer, TagSerializer
 
 
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    Представляет собой стандартный набор CRUD
+    для взаимодействия с сущностью `Tag`
+    """
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
 class ClientViewSet(viewsets.ModelViewSet):
+    """
+    Представляет собой стандартный набор CRUD
+    для взаимодействия с сущностью `Client`
+    """
+
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
 
 class MailingViewSet(viewsets.ModelViewSet):
+    """
+    Представляет собой стандартный набор CRUD
+    для взаимодействия с сущностью `Mailing`
+    """
+
     queryset = Mailing.objects.all()
     serializer_class = MailingSerializer
 
 
 class MessageViewSet(viewsets.ModelViewSet):
+    """
+    Представляет собой стандартный набор CRUD
+    для взаимодействия с сущностью `Message`
+    """
+
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-
-
-class TagViewSet(viewsets.ModelViewSet):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
